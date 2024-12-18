bd5dce947465ec517254ae8bfbd7f9eda9d33362
denmark_deni <mariadeniston111@gmail.com>
2024-12-18 12:50:51 +0530

    models.py changed

--- Vacationjob/nass/models.py
+++ Vacationjob/nass/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Register(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    first_name=models.CharField(max_length=15),
-    email=models.EmailField(max_length=30),
-    phonenumber=models.BigIntegerField(max_length=10),
+    first_name=models.CharField(max_length=15,default="user")
+    email=models.EmailField(max_length=30,default='abc@gmail.com')
+    phonenumber=models.BigIntegerField(default=1123)
     
